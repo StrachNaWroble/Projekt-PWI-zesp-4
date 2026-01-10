a5a446b10556e9007774347e6b2d350e8040d778
Adam Biel <bielik2007@gmail.com>
2026-01-10 19:14:05 +0100

    Poprawka w pliku cube.h - dopisanie metod: U(), R(), L(), D(), B(), F()

--- src_cpp/cube.h
+++ src_cpp/cube.h
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MOVES 4
+#define MOVES 6
 class cube
 {
 public:
@@ -10,6 +10,13 @@ public:
 	array<char, 12> eo; //orientacja krawedzi
 	//kostka jest reprezentowana wzgledem orientacji: niebieski center z przodu, biały center u góry, pomarańczowy center z prawej
 	cube();
+	
+	R();
+	L();
+	U();
+	D();
+	B();
+	F();
 
 	void move(int id);
 };

ff7c38dcdb4eaa38f74826ef460475977883b709
Adam Biel <bielik2007@gmail.com>
2026-01-10 23:36:58 +0100

    Naprawa poprawności tablicy generowanej przez eo_gen.cpp oraz opytmalizacja działania samego generatora.

--- src_cpp/eo_gen.cpp
+++ src_cpp/eo_gen.cpp
@@ -5,29 +5,48 @@ int main()
 	cube solved;
 	queue<pair<cube, int> > q;
 	q.push(make_pair(solved, 0));
-	auto custom_hash = [&](cube& obj)
+	auto to_int = [&](cube& obj)
 	{
 		int res = 0;
 		for(int i=0;i<obj.eo.size();i++)
 			res += obj.eo[i]*(1<<i);
 		return res;
 	};
-	vector<int> visited((1<<14), -1);
+	vector<int> opt((1<<14), 1e9);
+	map<array<int, 12>, int> visited;
+	int cnt = 0;
 	while(!q.empty())
 	{
+		cnt++;
+		if(cnt%100) debug(cnt);
+		if(cnt > 10000000) break;
 		auto [akt, dist] = q.front();
 		q.pop();
-		visited[custom_hash(akt)] = dist;
-		debug(akt.eo);
-		debug(custom_hash(akt));
+		opt[to_int(akt)] = min(dist, opt[to_int(akt)]);
+		visited[akt.ep] = 1;
 		for(int i=0;i<MOVES;i++)
 		{
 			cube nxt = akt;
 			nxt.move(i);
-			if(visited[custom_hash(nxt)] == -1)
+			if(visited[nxt.ep] == 0)
 				q.push(make_pair(nxt, dist+1));
 		}
 	}
-	for(int i=0;i<visited.size();i++)
-		cout<<visited[i]<<"\n";
+	for(int i=0;i<opt.size();i++)
+		cout<<opt[i]<<"\n";
+	for(int i=0;i<=4096;i++)
+	{
+		if(opt[i] == 1e9)
+		{
+			int s = 0;
+			for(int j=0;j<20;j++) 
+				if(i & (1<<j))
+					s++;
+			if(s%2 == 0)
+			{
+				cout<<":( "<<i;
+				return 0;
+			}
+		}
+	}
 }

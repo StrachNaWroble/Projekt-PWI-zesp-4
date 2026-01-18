a7740ae5dd2ea92370ea1bd006e50e47c52547c9
Adam Biel <bielik2007@gmail.com>
2026-01-18 18:11:35 +0100

    Dodanie klasy cph, której konstruktor ładuje dane heurystyki opartej na permutacji wierzchołków. Klasa udostepnia metode: get_cph(cube& state), która dla danego stanu kostki zwraca wartość heurystki.

---
+++ src_cpp/cph.cpp
@@ -0,0 +1,18 @@
+#include "cube.h"
+#include "cph.h"
+#include "lehmer_code.h"
+cph::cph()
+{
+	ifstream file("cph.txt");
+	if(!file) 
+	{
+		cerr<<"Nie wygenerowane danych cph!\n";
+		exit(0);
+	}
+	for(int i=0;i<40320;i++)
+		file>>_cph[i];
+}
+int cph::get_cph(cube& state)
+{
+	return _cph[lehmer_code(state)];	
+}

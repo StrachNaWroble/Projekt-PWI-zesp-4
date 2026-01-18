c0a6a720d641cdb3593c9308df769fd04911bca3
Adam Biel <bielik2007@gmail.com>
2026-01-18 20:02:30 +0100

    Zmiana struktury folderu src_cpp i dodanie skryptu kompilująego linkującego i uruchamiajacego generatory tablic heurystycznych.

---
+++ src_cpp/gen_heu_tabs.sh
@@ -0,0 +1,27 @@
+echo Rozpoczęcie kompilacji: 
+g++ -Iinclude cube.cpp -c 
+g++ -Iinclude lehmer_code.cpp -c 
+g++ -Iinclude eph_gen.cpp -c
+g++ -Iinclude eoh_gen.cpp -c
+g++ -Iinclude cph_gen.cpp -c
+
+echo Zakończenie kompilacji i rozpoczęcie linkowania:
+g++ cube.o lehmer_code.o eph_gen.o -o eph_gen
+g++ cube.o lehmer_code.o eoh_gen.o -o eoh_gen
+g++ cube.o lehmer_code.o cph_gen.o -o cph_gen
+echo Zakończenie linkowania
+
+echo Rozpoczęcie generowania tablicy eph:
+./eph_gen >../data/eph.txt
+
+echo Rozpoczęcie generowania tablicy eoh:
+./eoh_gen >../data/eoh.txt
+
+echo Rozpoczęcie generowania tablicy cph:
+./cph_gen >../data/cph.txt
+
+rm *.o
+rm cph_gen
+rm eph_gen
+rm eoh_gen
+

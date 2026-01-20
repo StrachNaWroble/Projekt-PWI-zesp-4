2f4b0f8c593fafee82ae0a9958fd658709997eed
Adam Biel <bielik2007@gmail.com>
2026-01-20 15:27:20 +0100

    Dodanie skryptów do kompilacji: compile.sh i link.sh.

---
+++ src_cpp/compile.sh
@@ -0,0 +1,14 @@
+echo kompilacja lehmer_code.cpp:
+g++ -Iinclude lehmer_code.cpp -c
+echo kompilacja eph.cpp
+g++ -Iinclude eph.cpp -c
+echo kompilacja cph.cpp
+g++ -Iinclude cph.cpp -c
+echo kompilacja eoh.cpp
+g++ -Iinclude eoh.cpp -c
+echo kompilacja cube.cpp
+g++ -Iinclude cube.cpp -c
+echo kompilacja ida_star.cpp
+g++ -Iinclude ida_star.cpp -c
+echo kompilacja main.cpp
+g++ -Iinclude main.cpp -c
---
+++ src_cpp/link.sh
@@ -0,0 +1,2 @@
+echo linkowanie
+g++ -Iinclude ida_star.o eph.o cph.o eoh.o lehmer_code.o cube.o main.o -o main
